398071479d9a8a853fdc36bb9d038a7af41dd208
Mohamedyasser1100 <mohamedyasser9933@gmail.com>
2024-12-22 18:42:51 +0200

    all expenses header part 1

---
+++ lib/widgets/all_expenses_header.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AllExpenssesHeader extends StatelessWidget {
+  const AllExpenssesHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [],
+    );
+  }
+}
