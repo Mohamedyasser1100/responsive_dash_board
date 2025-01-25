325bccef642e26a8f0466e363949b54ed4461160
Mohamedyasser1100 <mohamedyasser9933@gmail.com>
2025-01-25 21:46:16 +0200

    quick invoice part one

---
+++ lib/widgets/quick_invoice.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/custome_background_container.dart';
+
+class QuickInvoice extends StatelessWidget {
+  const QuickInvoice({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomeBackgroundContainer(child: SizedBox());
+  }
+}
