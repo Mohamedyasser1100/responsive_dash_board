96b9ccbcf16e1e8630c6497719b0955707eef094
Mohamedyasser1100 <mohamedyasser9933@gmail.com>
2025-01-28 15:34:23 +0200

    transaction history part 2

--- lib/widgets/transaction_history.dart
+++ lib/widgets/transaction_history.dart
@@ -7,19 +7,40 @@ class TransactionHistory extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              'Transaction History',
-              style: AppStyle.styleRegular20,
-            ),
-            Text(
-              'See all',
-              style: AppStyle.styleMeduim16.copyWith(color: Color(0xff4EB7F2)),
-            )
-          ],
+        TransactionHistoryHeader(),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          '24 Feb 2024',
+          style: AppStyle.styleRegular16,
+        )
+      ],
+    );
+  }
+}
+
+class TransactionHistoryHeader extends StatelessWidget {
+  const TransactionHistoryHeader({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'Transaction History',
+          style: AppStyle.styleRegular20,
+        ),
+        Text(
+          'See all',
+          style: AppStyle.styleMeduim16.copyWith(
+            color: Color(0xff4EB7F2),
+          ),
         )
       ],
     );
