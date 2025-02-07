186de8ab6bcee14e849e8577c0ce48c36ef967a3
Mohamedyasser1100 <mohamedyasser9933@gmail.com>
2025-02-07 15:23:44 +0200

    detailed income chart part 2

--- lib/widgets/income_section_body.dart
+++ lib/widgets/income_section_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/utils/size_config.dart';
+import 'package:responsive_dash_board/widgets/detailed_income_chart.dart';
 import 'package:responsive_dash_board/widgets/income_chart.dart';
 import 'package:responsive_dash_board/widgets/income_details.dart';
 
@@ -12,7 +13,11 @@ class IncomeSectionBody extends StatelessWidget {
   Widget build(BuildContext context) {
     double width = MediaQuery.sizeOf(context).width;
     return width >= SizeConfig.desktop && width < 1532
-        ? const Expanded(child: IncomeChart())
+        ? const Expanded(
+            child: Padding(
+            padding: EdgeInsets.all(16),
+            child: DetailedIncomeChart(),
+          ))
         : const Row(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
